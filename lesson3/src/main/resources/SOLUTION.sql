f332795604fb2d44d5ddf7ba10419bd02ab2ce75
DmitryAnisimovJava <DmitryAnisimovJava@users.noreply.github.com>
2024-07-21 19:28:30 +0300

    Last changes

--- lesson3/src/main/resources/SOLUTION.sql
+++ lesson3/src/main/resources/SOLUTION.sql
@@ -1,9 +1,17 @@
 ALTER TABLE student ALTER COLUMN birthday SET NOT NULL;
 
-ALTER TABLE mark ADD CONSTRAINT mark_constraint CHECK (mark >= 1 AND mark <= 10), ALTER COLUMN student_id SET NOT NULL, ALTER COLUMN subject_id SET NOT NULL;
+ALTER TABLE mark ADD CONSTRAINT mark_constraint CHECK (mark >= 1 AND mark <= 10);
 
-ALTER TABLE subject ADD CONSTRAINT grade_range_constraint CHECK ( grade >= 1 AND grade <= 5);
+ALTER TABLE mark ALTER COLUMN student_id SET NOT NULL;
 
-ALTER TABLE paymenttype ADD UNIQUE (name);
+ALTER TABLE mark ALTER COLUMN subject_id SET NOT NULL;
 
-ALTER TABLE payment ALTER COLUMN type_id SET NOT NULL, ALTER COLUMN amount SET NOT NULL, ALTER COLUMN payment_date SET NOT NULL;
+ALTER TABLE subject ADD CONSTRAINT grade_range_constraint CHECK (grade >= 1 AND grade <= 5);
+
+ALTER TABLE paymenttype ADD CONSTRAINT Unique_name UNIQUE (name);
+
+ALTER TABLE payment ALTER COLUMN type_id SET NOT NULL;
+
+ALTER TABLE payment ALTER COLUMN amount SET NOT NULL;
+
+ALTER TABLE payment ALTER COLUMN payment_date SET NOT NULL;
